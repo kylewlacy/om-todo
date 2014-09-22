f0231c3f197658e9497b55fb8d2a59b7cd503ba6
Kyle Lacy <kylewlacy@me.com>
2014-09-22 16:51:49 -0700

    Add list with one to-do item

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -4,13 +4,29 @@
             [domina        :as d]
             [domina.events :as e]))
 
-(def app-state (atom {:text "Hello wrold!"}))
+(enable-console-print!)
 
-(defn todo-list [data owner]
+
+
+(defn list-item [item]
+  [:li
+    [:input {:type "checkbox"
+             :checked (if (:completed? item) "checked" "")}]
+    [:span (:title item)]])
+
+(defn list-items [items]
+  [:ol#todo-list (map list-item items)])
+
+(defn todo-list [app owner]
   (om/component
-    (html [:h1 (:text data)])))
+    (html
+      [:section#to-do
+        [:h1 "To-Do List"]
+        (list-items (:items app))])))
 
 
+
+(def app-state (atom {:items [{:title "Hello wrold!"}]}))
 
 (defn setup []
   (om/root todo-list app-state {:target (d/by-id "app")}))
